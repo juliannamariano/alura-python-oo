9d84c457b02940d1ec19814c33474dc2b17c1932
Julianna Mariano <juliannamariano27@outlook.com>
2025-06-10 15:36:08 -0300

    criando a classe avaliação

--- oo-python/modelos/restaurante.py
+++ oo-python/modelos/restaurante.py
@@ -1,3 +1,5 @@
+
+from modelos.avaliacao import Avaliacao
 class Restaurante:
     restaurantes = []
 
@@ -9,6 +11,7 @@ class Restaurante:
         self.categoria = categoria.upper()
         #com o "_" deixa o nosso atributo privado
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -17,7 +20,7 @@ class Restaurante:
     @classmethod
     def listar_restaurantes(cls):
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante._nome} | {restaurante.categoria} | {restaurante.media_avaliacoes} | {restaurante.ativo} | ')
 
     @property
     def ativo(self):
@@ -26,11 +29,23 @@ class Restaurante:
     def alternar_estado(self):
         self._ativo = not self._ativo
 
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum (avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
+
 #restaurante_praca = Restaurante('Praça', 'Gourmet')
 #restaurante_praca.alternar_estado()
 #restaurante_pizza = Restaurante('Pizza Express', 'Italiano')
 
 ##restaurantes = [restaurante_praca, restaurante_pizza]
 #print(restaurante_praca)
-
-Restaurante.listar_restaurantes()
+#Restaurante.listar_restaurantes()
