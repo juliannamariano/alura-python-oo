407e688868e69937ccebbf1350680c31f05b4a7c
Julianna Mariano <juliannamariano27@outlook.com>
2025-06-10 15:39:33 -0300

    update avaliação

--- oo-python/app.py
+++ oo-python/app.py
@@ -2,9 +2,8 @@
 from modelos.restaurante import Restaurante 
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 5)
+#restaurante_praca.receber_avaliacao('Gui', 10)
+##restaurante_praca.receber_avaliacao('Emy', 5)
 #restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 #restaurante_japones = Restaurante('Japa', "Japonesa")
 
