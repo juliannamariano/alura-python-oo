33bd9a4dc345aeb8689a12280457f76c599b96dd
Julianna Mariano <juliannamariano27@outlook.com>
2025-06-12 10:21:55 -0300

    Criando o método abstrato e utilizando ele nas classes
    Criando o método abstrato e dentro das classes filhas adicionando esse método dentro realizando o polimorfismo

--- oo-python/modelos/cardapio/item_cardapio.py
+++ oo-python/modelos/cardapio/item_cardapio.py
@@ -1,4 +1,12 @@
-class ItemCardapio:
+#import para classe abstrata
+from abc import ABC, abstractmethod
+
+class ItemCardapio(ABC):
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
+
+    # Montando uma classe abstrata tirando a repetição de código, lembrando que essa classe não pode ser instanciada
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
